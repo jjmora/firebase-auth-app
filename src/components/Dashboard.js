76c050e83e00944aabd28a35150d4c856a39f0d3
JuanJoseMora <juanmora.dev@gmail.com>
2021-09-12 14:43:41 +0200

    ready for deploy in Github

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -26,7 +26,7 @@ export default function Dashboard() {
         <div>
           <Card>
             <Card.Body>
-              <h2 className="text-center mb-4">Profile</h2>
+              <h2 className="text-center mb-4">Dashboard</h2>
               {error && <Alert variant="danger">{error}</Alert>}
               <strong>Email: </strong>{currentUser && currentUser.email}
               <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
